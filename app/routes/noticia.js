4397fc28450150865b6e5bb21720f7458b704c01
Julio2897 <julioferro28@gmail.com>
2018-07-13 15:24:11 -0300

    Implementando as models para noticias  e noticia

--- app/routes/noticia.js
+++ app/routes/noticia.js
@@ -5,9 +5,9 @@ module.exports = function(app){
 
 		//Executa a funcao que realiza a conexao
 		var connection = app.config.dbConnection(); //agora vai executar somente quando a rota for requisitada
+		var noticiasModel = app.app.models.noticiasModel;
 
-		//executando uma chamada de acesso ao banco de dados
-		connection.query('select * from noticias where id_noticias = 1', function(error, result){
+		noticiasModel.getNoticia(connection, function(error, result){
 			res.render("noticias/noticia", {noticia : result});
 		});
 	});
--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -5,9 +5,10 @@ module.exports = function(app){
 
 		//Executa a funcao que realiza a conexao
 		var connection = app.config.dbConnection(); //agora vai executar somente quando a rota for requisitada
+		var noticiasModel = app.app.models.noticiasModel;
 
 		//executando uma chamada de acesso ao banco de dados
-		connection.query('select * from noticias', function(error, result){
+		noticiasModel.getNoticias(connection, function(error, result){
 			res.render("noticias/noticias", {noticias : result});
 		});
 	});
