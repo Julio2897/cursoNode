d0438dcc12ad5c603ce9c8619127f7efd79e1bb6
Julio2897 <julioferro28@gmail.com>
2018-07-13 13:42:22 -0300

    Criando conexão com banco de dados mysql

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,6 +1,19 @@
 module.exports = function(app){	
-	app.get('/noticias', function(req,res){
 
-		res.render("noticias/noticias");
+	app.get('/noticias', function(req,res){
+		
+		//incorporando o modulo mysql a aplicacao
+		var mysql = require('mysql');
+		//estabelecendo a conexao
+		var connection = mysql.createConnection({
+			host: 'localhost', //endereco da conexao
+			user: 'root',
+			password: 'root',
+			database: 'portal_noticias'
+		});
+		//executando uma chamada de acesso ao banco de dados
+		connection.query('select * from noticias', function(error, result){
+			res.render("noticias/noticias", {noticias : result});
+		});
 	});
 };
